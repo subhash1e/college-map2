df62e9c851212e21f09513ced4783a07ec3897b6
Subhash Kumar <85139394+subhash1e@users.noreply.github.com>
2022-10-01 11:04:51 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+
+function initMap(){
+    map = new google.maps.Map(document.getElementById('map'),
+    {
+        center: {lat: 23.5477,lng:  87.2931},
+        zoom: 17, 
+        mapId: '4adeedb30573ba09'
+    });
+
+
+};
